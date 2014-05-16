8b5237ee16cdabc6adc7e0a375b3182e0acae4de
anthonyhombiat <anthonyhombiat@51e108a9-9e07-433b-b235-c056ae316712>
2014-05-16 15:09:42 +0000

    Create a TagInfoRequest object.
    git-svn-id: svn+ssh://scm.forge.imag.fr/var/lib/gforge/chroot/scmrepos/svn/cwb@13 51e108a9-9e07-433b-b235-c056ae316712

--- trunk/cwb/src/main/java/lig/steamer/cwb/util/wsclient/taginfo/TagInfoClient.java
+++ trunk/cwb/src/main/java/lig/steamer/cwb/util/wsclient/taginfo/TagInfoClient.java
@@ -1,11 +1,10 @@
-package lig.steamer.cwb.util;
+package lig.steamer.cwb.util.wsclient.taginfo;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
-import java.net.URISyntaxException;
 import java.net.URL;
 import java.util.Locale;
 import java.util.logging.Level;
@@ -20,27 +19,18 @@ import lig.steamer.cwb.tagging.model.impl.LocalizedString;
 import lig.steamer.cwb.tagging.model.impl.Tag;
 import lig.steamer.cwb.tagging.model.impl.TagSet;
 
-import org.apache.http.client.utils.URIBuilder;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.JSONValue;
 
 /**
- * @author Anthony Hombiat
- * OSM TagInfo client that provides methods to consume data from the restful Web Service OSM TagInfo API.
+ * @author Anthony Hombiat OSM TagInfo client that provides methods to consume
+ * data from the restful Web Service OSM TagInfo API.
  */
 public class TagInfoClient {
 
 	private static Logger LOGGER = Logger
 			.getLogger("lig.steamer.cwb.io.taginfo");
-
-	private static String TAGINFO_QUERY_BY_KEY_URL = "http://taginfo.openstreetmap.org/api/4/key/values";
-	private static String TAGINFO_QUERY_PARAMETER_LANG = "lang";
-	private static String TAGINFO_QUERY_PARAMETER_SORT_BY = "sortname";
-	private static String TAGINFO_QUERY_PARAMETER_SORT_ORDER = "sortorder";
-	private static String TAGINFO_QUERY_PARAMETER_PAGE_NB = "page";
-	private static String TAGINFO_QUERY_PARAMETER_RESULTS_PER_PAGE = "rp";
-	private static String TAGINFO_QUERY_PARAMETER_KEY = "key";
 
 	private static String TAGINFO_RESULT_ARRAY_KEY = "data";
 	private static String TAGINFO_RESULT_VALUE_KEY = "value";
@@ -50,7 +40,7 @@ public class TagInfoClient {
 
 	private static String DEFAULT_LOCALE = Locale.ENGLISH.toString();
 	private static double DEFAULT_THRESHOLD = 0;
-	
+
 	public static String OSM_TAG_INFO_URI = "http://osmtaginfo";
 
 	public TagInfoClient() {
@@ -58,11 +48,11 @@ public class TagInfoClient {
 	}
 
 	/**
-	 * Returns the TagSet that gathers OSM tags corresponding to the given key 
-	 * 		for the given locale and for the given frequency of occurence threshold.
-	 * @param key, the key
-	 * @param locale, the locale
-	 * @param threshold, the threshold
+	 * Returns the TagSet that gathers OSM tags corresponding to the given key
+	 * for the given locale and for the given frequency of occurence threshold.
+	 * @param key , the key
+	 * @param locale , the locale
+	 * @param threshold , the threshold
 	 * @return the TagSet
 	 */
 	public ITagSet getTagsByKey(String key, String locale, double threshold) {
@@ -71,21 +61,10 @@ public class TagInfoClient {
 
 		try {
 
-			URIBuilder uriBuilder = new URIBuilder(TAGINFO_QUERY_BY_KEY_URL);
-			uriBuilder
-					.setParameter(TAGINFO_QUERY_PARAMETER_LANG, locale)
-					.setParameter(TAGINFO_QUERY_PARAMETER_SORT_BY,
-							"count_all")
-					.setParameter(TAGINFO_QUERY_PARAMETER_SORT_ORDER, "desc")
-					.setParameter(TAGINFO_QUERY_PARAMETER_PAGE_NB, "1")
-					.setParameter(TAGINFO_QUERY_PARAMETER_RESULTS_PER_PAGE,
-							"10")
-					.setParameter(TAGINFO_QUERY_PARAMETER_KEY, key);
+			TagInfoRequest tagInfoRequest = new TagInfoRequest(key, locale);
+			URL url = tagInfoRequest.getUrl();
 
-			URL tagInforURL = uriBuilder.build().toURL();
-			System.out.println(tagInforURL.toString());
-			HttpURLConnection connection = (HttpURLConnection) tagInforURL
-					.openConnection();
+			HttpURLConnection connection = (HttpURLConnection) url.openConnection();
 			connection.setRequestMethod(HttpMethod.GET.toString());
 			connection.setRequestProperty(HttpRequest.ACCEPT.toString(),
 					HttpRequest.APPLICATION_JSON.toString());
@@ -103,9 +82,9 @@ public class TagInfoClient {
 			while ((line = br.readLine()) != null) {
 				stringBuilder.append(line);
 			}
-			
+
 			System.out.println(stringBuilder.toString());
-			
+
 			JSONObject obj = (JSONObject) JSONValue.parse(stringBuilder
 					.toString());
 			JSONArray array = (JSONArray) obj.get(TAGINFO_RESULT_ARRAY_KEY);
@@ -144,8 +123,6 @@ public class TagInfoClient {
 			e.printStackTrace();
 		} catch (IOException e) {
 			e.printStackTrace();
-		} catch (URISyntaxException e) {
-			e.printStackTrace();
 		}
 
 		return null;
@@ -162,44 +139,45 @@ public class TagInfoClient {
 	}
 
 	/**
-	 * Returns the TagSet that gathers OSM tags corresponding to the given key for the given locale.
+	 * Returns the TagSet that gathers OSM tags corresponding to the given key
+	 * for the given locale.
 	 * @param key
 	 * @return the TagSet
 	 */
 	public ITagSet getTagsByKey(String key, String locale) {
 		return this.getTagsByKey(key, locale, DEFAULT_THRESHOLD);
 	}
-	
+
 	/**
 	 * Returns the TagSet that gathers OSM tags corresponding to the given keys.
-	 * @param keys, the keys
+	 * @param keys , the keys
 	 * @return the TagSet
 	 */
-	public ITagSet getTagsByKeys(String [] keys, String locale, double threshold){
+	public ITagSet getTagsByKeys(String[] keys, String locale, double threshold) {
 		ITagSet tagSet = new TagSet();
-		for(String key:keys){
+		for (String key : keys) {
 			tagSet.mergeTagSet(getTagsByKey(key, locale, threshold));
 		}
 		return tagSet;
 	}
-	
+
 	/**
-	 * Returns the TagSet that gathers OSM tags corresponding to the given keys 
-	 * 		for the given locale.
-	 * @param keys, the keys
-	 * @param locale, the locale
+	 * Returns the TagSet that gathers OSM tags corresponding to the given keys
+	 * for the given locale.
+	 * @param keys , the keys
+	 * @param locale , the locale
 	 * @return the TagSet
 	 */
-	public ITagSet getTagsByKeys(String [] keys, String locale){
+	public ITagSet getTagsByKeys(String[] keys, String locale) {
 		return getTagsByKeys(keys, locale, DEFAULT_THRESHOLD);
 	}
-	
+
 	/**
 	 * Returns the TagSet that gathers OSM tags corresponding to the given keys.
-	 * @param keys, the keys
+	 * @param keys , the keys
 	 * @return the TagSet
 	 */
-	public ITagSet getTagsByKeys(String [] keys){
+	public ITagSet getTagsByKeys(String[] keys) {
 		return getTagsByKeys(keys, DEFAULT_LOCALE, DEFAULT_THRESHOLD);
 	}
 
